03aa9e82141ce6f5f3e5b872b544ffe270b015d3
Mina <mina_zarkesh@gmx.de>
2023-10-23 21:40:01 +0200

    mehr objekte erstellt

---
+++ js/models/drawable-object.class.js
@@ -0,0 +1,48 @@
+class DrawableObject {
+  //   img;
+  //   imageCache = {};
+  //   currentImage = 0;
+  //   x = 120;
+  //   y = 250;
+  //   fps = (60 / 12);
+  //   height = 150;
+  //   width = 100;
+  //   ctx;
+  //   offsetX = 0;
+  //   offsetY=0;
+  //   offsetWidth=0;
+  //   offsetHeight=0;
+
+  //     // load Image('img/test.png');
+  // loadImage(path) {
+  //   this.img = new Image();
+  //   this.img.src = path;
+  // }
+  // draw(ctx) {
+  //   ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  // }
+
+  // drawFrame(ctx) {
+  //   if (this instanceof Character || this instanceof Chicken || this instanceof CollectableObject) {
+  //     ctx.beginPath();
+  //     ctx.lineWidth = "5";
+  //     ctx.strokeStyle = "blue";
+  //     ctx.rect(this.x+this.offsetX, this.y+this.offsetY, this.width-this.offsetWidth, this.height-this.offsetHeight);
+  //     ctx.stroke();
+  //   }
+  // }
+
+  
+  // /**
+  //  *
+  //  * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+  //  */
+  // loadImages(arr) {
+  //   arr.forEach((path) => {
+  //     let img = new Image();
+  //     img.src = path;
+  //     this.imageCache[path] = img;
+  //   });
+  // }
+
+}
